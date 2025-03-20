ff541490fffabb9de381a376d45c68666d00ca60
ThiagoHenriquue <th14henriquue@gmail.com>
2025-03-20 14:00:58 -0300

    wrapper.

---
+++ Tarefa_Wrapper/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        short num1 = 1111;
+        System.out.println(num1);
+        Short num2 = num1;
+        System.out.println(num2);
+
+
+
+
+    }
+}
